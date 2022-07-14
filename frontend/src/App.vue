1a3c956161c06a5354817b6ffe72a9067985a704
yeonsuBeak <soothbugger@gmail.com>
2022-07-14 22:16:07 +0900

    🚧 Import MainContent to App and mark up it

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <GlobalHeader />
+    <MainContent />
   </div>
 </template>
 
 <script>
 import GlobalHeader from "./components/GlobalHeader.vue";
+import MainContent from "./components/MainContent.vue";
 
 export default {
   name: "App",
   components: {
     GlobalHeader,
+    MainContent,
   },
 };
 </script>
---
+++ frontend/src/components/MainContent.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="current">
+            <p class="temp-summery">
+              <span class="temp-highest" aria-label="최고 기온">29</span>
+              <span class="temp-lowest" aria-label="최저 기온">24</span>
+            </p>
+            <strong class="temp-current" aria-label="현재 기온">26</strong>
+            <div class="current-image">
+              <img src="../assets/icon-clear.svg" alt="대체로 맑음" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "MainContent",
+};
+</script>
