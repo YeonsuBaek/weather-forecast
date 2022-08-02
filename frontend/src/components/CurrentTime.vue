12ca7184b7dc953cfdd30b7a21c0253054bfeb7e
yeonsuBeak <soothbugger@gmail.com>
2022-08-02 20:45:29 +0900

    🚧 Display the current date and time

--- frontend/src/components/CurrentTime.vue
+++ frontend/src/components/CurrentTime.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="current-time">
-    <strong class="date">6.27</strong>
-    <span class="time">21:35</span>
+    <div class="wrapper">
+      <strong class="date">{{ month }}.{{ day }}</strong>
+      <span class="time">{{ hour }}:{{ minute }}</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "CurrentTime",
+  data() {
+    return {
+      month: "00",
+      day: "00",
+      hour: "00",
+      minute: "00",
+    };
+  },
+  created() {
+    this.setCurrentTime();
+  },
+
+  methods: {
+    setCurrentTime() {
+      let today = new Date();
+      let month = ("0" + (today.getMonth() + 1)).slice(-2);
+      let day = ("0" + today.getDate()).slice(-2);
+      let hour = ("0" + today.getHours()).slice(-2);
+      let minute = ("0" + today.getMinutes()).slice(-2);
+
+      this.month = month;
+      this.day = day;
+      this.hour = hour;
+      this.minute = minute;
+    },
+  },
 };
 </script>
