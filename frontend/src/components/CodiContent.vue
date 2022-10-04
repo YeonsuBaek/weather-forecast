f55a337eb0300102772de864e3f386cbb06111ab
yeonsuBeak <soothbugger@gmail.com>
2022-10-04 17:07:34 +0900

    ✅ Add codi-items to codi-list

--- frontend/src/components/CodiContent.vue
+++ frontend/src/components/CodiContent.vue
@@ -6,34 +6,23 @@
           <div class="col-sm-4">
             <header class="codi-header">
               <h2 class="codi-title">여성 코디 추천</h2>
-              <a class="codi-site-button" href="#">더보기</a>
+              <a
+                class="codi-site-button"
+                href="https://store.musinsa.com/app/codimap/lists"
+                target="_blank"
+                >더보기</a
+              >
             </header>
           </div>
-
-          <div class="col-sm-2">
-            <a class="codi-item" href="#">
-              <img src="../assets/images/img-wcodi-01.png" alt="여성 코디 1" />
-            </a>
-          </div>
-
-          <div class="col-sm-2">
-            <a class="codi-item" href="#">
-              <img src="../assets/images/img-wcodi-02.png" alt="여성 코디 2" />
-            </a>
-          </div>
-
-          <div class="col-sm-2">
-            <a class="codi-item" href="#">
-              <img src="../assets/images/img-wcodi-03.png" alt="여성 코디 3" />
-            </a>
-          </div>
-
-          <div class="col-sm-2">
-            <a class="codi-item" href="#">
-              <img src="../assets/images/img-wcodi-04.png" alt="여성 코디 4" />
-            </a>
-          </div>
         </div>
+
+        <ul class="row codi-list">
+          <li v-for="(codi, index) in women" :key="index" class="col-sm-2">
+            <a class="codi-item" :href="codi.link" target="_blank">
+              <img :src="codi.image" :alt="codi.alt" />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
 
@@ -43,41 +32,43 @@
           <div class="col-sm-4">
             <header class="codi-header">
               <h2 class="codi-title">남성 코디 추천</h2>
-              <a class="codi-site-button" href="#">더보기</a>
+              <a
+                class="codi-site-button"
+                href="https://store.musinsa.com/app/codimap/lists"
+                target="_blank"
+                >더보기</a
+              >
             </header>
           </div>
-
-          <div class="col-sm-2">
-            <a class="codi-item" href="#">
-              <img src="../assets/images/img-mcodi-01.png" alt="남성 코디 1" />
-            </a>
-          </div>
-
-          <div class="col-sm-2">
-            <a class="codi-item" href="#">
-              <img src="../assets/images/img-mcodi-02.png" alt="남성 코디 2" />
-            </a>
-          </div>
-
-          <div class="col-sm-2">
-            <a class="codi-item" href="#">
-              <img src="../assets/images/img-mcodi-03.png" alt="남성 코디 3" />
-            </a>
-          </div>
-
-          <div class="col-sm-2">
-            <a class="codi-item" href="#">
-              <img src="../assets/images/img-mcodi-04.png" alt="남성 코디 4" />
-            </a>
-          </div>
         </div>
+
+        <ul class="row codi-list">
+          <li v-for="(codi, index) in men" :key="index" class="col-sm-2">
+            <a class="codi-item" :href="codi.link" target="_blank">
+              <img :src="codi.image" :alt="codi.alt" />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-export default {};
+import womenCodi from "@/assets/data/women.json";
+import menCodi from "@/assets/data/men.json";
+
+export default {
+  setup() {
+    const women = womenCodi;
+    const men = menCodi;
+
+    return {
+      women,
+      men,
+    };
+  },
+};
 </script>
 
 <style></style>
